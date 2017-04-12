5f63f77181c993f957cf017c5dd0f51a066397dc
drworm75 <dwgames@gmail.com>
2017-04-11 19:55:29 -0500

    Examples done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,74 @@
-$('#test').on('click', function () {
-	alert("JQuery Works!");
+$(document).ready(function() {
+	$('#test').on('click', function (e) {
+		console.log("event",event)
+		console.log("this", $(this));
+		console.log("this not jq", this)
+	});
 });
+
+// this.a = 37
+// console.log(window.a);
+
+
+// var fubar = "42";
+
+// function baz() {
+// 	console.log("baz");
+// 	bar();
+// }
+
+// function bar() {
+// 	console.log("bar");
+// 	foo();
+// }
+
+// function foo() {
+// 	console.log("foo");
+// 	var fubar = 13;
+// 	console.log("this", this); //  foo
+// 	console.log("fubar", this.fubar); // 13
+// }
+
+// baz();
+
+var a = 3;
+
+function foo() {
+	console.log("this", this); // obj
+	for (key in this) {
+		console.log(this.key);
+	} 
+	console.log("a", this.monkey); // 2
+}
+
+var obj = {
+	a: 2,
+	monkey: foo
+};
+
+obj.monkey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
